ad5d3f1d1843ba97f67a00eaae12b358c7a056f8
leebrouse <128211841+leebrouse@users.noreply.github.com>
2024-02-28 18:27:42 +0800

    complete part1

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -17,11 +17,58 @@ public class TimeAList {
         }
     }
 
+    public static double times(int N){
+        Stopwatch t=new Stopwatch();
+        AList<Integer> p=new AList<>();
+        for (int i=0;i<N;i++){
+            p.addLast(i);
+        }
+        double timeInSeconds = t.elapsedTime();
+        return timeInSeconds;
+    }
+
     public static void main(String[] args) {
         timeAListConstruction();
     }
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+            System.out.println("Timing table for addLast");
+            AList<Integer> Ns=new AList<>();
+            Ns.addLast(1000);
+            Ns.addLast(2000);
+            Ns.addLast(4000);
+            Ns.addLast(8000);
+            Ns.addLast(16000);
+            Ns.addLast(32000);
+            Ns.addLast(64000);
+            Ns.addLast(128000);
+
+            AList<Double> times=new AList<>();
+            //computer times
+            times.addLast(times(1000));
+            times.addLast(times(2000));
+            times.addLast(times(4000));
+            times.addLast(times(8000));
+            times.addLast(times(16000));
+            times.addLast(times(32000));
+            times.addLast(times(64000));
+            times.addLast(times(128000));
+
+
+            AList<Integer> opCounts =new AList<>();
+            opCounts.addLast(1000);
+            opCounts.addLast(2000);
+            opCounts.addLast(4000);
+            opCounts.addLast(8000);
+            opCounts.addLast(16000);
+            opCounts.addLast(32000);
+            opCounts.addLast(64000);
+            opCounts.addLast(128000);
+
+            printTimingTable(Ns,times,opCounts);
+
+
+
     }
 }
