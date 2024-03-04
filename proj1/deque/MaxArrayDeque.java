9978868ef2ff89f9ac9e6ce2bd0a7294383fea76
leebrouse <128211841+leebrouse@users.noreply.github.com>
2024-03-04 22:09:32 +0800

    complete Maxarrydeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,7 +1,40 @@
 package deque;
 
-public class MaxArrayDeque<Leebrosue> extends ArrayDeque<Leebrosue>{
-    ArrayDeque<Integer> t=new ArrayDeque<>();
+import java.util.Comparator;
 
+public class MaxArrayDeque<Leebrosue> extends ArrayDeque<Leebrosue>{
+    private final Comparator<Leebrosue> comparator;
+    public MaxArrayDeque(Comparator<Leebrosue> c){
+        super();
+        comparator=c;
+    }
+
+    public Leebrosue max(){
+        if(isEmpty()){
+            return null;
+        }
+
+        Leebrosue t=get(0);
+        for (int c=0;c<size();c++){
+             if ((comparator.compare(get(c),t))>0){
+                 t=get(c);
+             }
+        }
+        return t;
+    }
+
+    public Leebrosue max(Comparator<Leebrosue> c){
+        if (isEmpty()){
+            return null;
+        }
+
+        Leebrosue p=get(0);
+        for (int t=0;t<size();t++){
+            if ((c.compare(get(t),p))>0){
+                p=get(t);
+            }
+        }
+        return p;
+    }
 
 }
---
+++ proj1/deque/MaxArrayDequeTest.java
@@ -0,0 +1,4 @@
+package deque;
+
+//public class MaxArrayDequeTest {
+//}
