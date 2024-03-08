b58574127d5287f8c9b26d7298c5cae26b845740
leebrouse <128211841+leebrouse@users.noreply.github.com>
2024-03-08 20:05:22 +0800

    complete proj1ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -19,7 +19,7 @@ public class TestArrayDequeEC {
                         student.addFirst(i);
                         Integer expect=solution.get(0);
                         Integer actual=student.get(0);
-                        assertEquals("Error in addFirst,student is "+actual+",correct is "+expect,expect,actual);
+                        assertEquals("student is "+actual+",correct is "+expect,expect,actual);
                         break;
 
                 case 1:
@@ -27,19 +27,19 @@ public class TestArrayDequeEC {
                         student.addLast(i);
                         Integer expect1=solution.get(solution.size()-1);
                         Integer actual1=student.get(student.size()-1);
-                        assertEquals("Error in addFirst,student is "+actual1+",correct is "+expect1,expect1,actual1);
+                        assertEquals("student is "+actual1+",correct is "+expect1,expect1,actual1);
                         break;
 
                 case 2:
                     Integer expect2=solution.size() > 0 ? solution.removeFirst() : null;
                     Integer actual2=student.size() > 0 ? solution.removeFirst() : null;
-                    assertEquals("Error in removeFirst,student is "+actual2+",correct is "+expect2,expect2,actual2);
+                    assertEquals("student is "+actual2+",correct is "+expect2,expect2,actual2);
                     break;
 
                 case 3:
                     Integer expect3=solution.size() > 0 ? solution.removeLast() : null;
                     Integer actual3=student.size() > 0 ? solution.removeLast() : null;
-                    assertEquals("Error in removeLast,student is "+actual3+",correct is "+expect3,expect3,actual3);
+                    assertEquals("student is "+actual3+",correct is "+expect3,expect3,actual3);
                     break;
 
             }
