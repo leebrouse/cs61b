b2f6c67a88a56f052473368708538919bb389852
leebrouse <128211841+leebrouse@users.noreply.github.com>
2024-03-08 22:05:06 +0800

    complete proj1ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -4,42 +4,62 @@ import static org.junit.Assert.*;
 import edu.princeton.cs.introcs.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestArrayDequeEC {
+    private String ErrorShow(List<String> strings){
+        StringBuilder result=new StringBuilder();
+        for (String items:strings){
+            result.append(items);
+            result.append("\n");
+        }
+        return  result.toString();
+    }
+
     @Test
     public void ErrorTest(){
         StudentArrayDeque<Integer> student= new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> solution=new ArrayDequeSolution<>();
+        List<String> strings=new ArrayList<>();
 
         for(int i=0;i<100;i++){
             int MethodSelector = StdRandom.uniform(4);
             switch (MethodSelector){
                 case 0:
+                        strings.add("addFirst("+i+")");
                         solution.addFirst(i);
                         student.addFirst(i);
                         Integer expect=solution.get(0);
                         Integer actual=student.get(0);
-                        assertEquals("student is "+actual+",correct is "+expect,expect,actual);
+                        assertEquals(ErrorShow(strings),expect,actual);
                         break;
 
                 case 1:
+                        strings.add("addLast("+i+")");
                         solution.addLast(i);
                         student.addLast(i);
                         Integer expect1=solution.get(solution.size()-1);
                         Integer actual1=student.get(student.size()-1);
-                        assertEquals("student is "+actual1+",correct is "+expect1,expect1,actual1);
+                        assertEquals(ErrorShow(strings),expect1,actual1);
                         break;
 
                 case 2:
-                    Integer expect2=solution.size() > 0 ? solution.removeFirst() : null;
-                    Integer actual2=student.size() > 0 ? solution.removeFirst() : null;
-                    assertEquals("student is "+actual2+",correct is "+expect2,expect2,actual2);
+                    if (solution.size()>0){
+                        strings.add("removeFirst()") ;
+                    }
+
+                    assertEquals(solution.size() > 0 ? solution.removeFirst() : null
+                            ,student.size() > 0 ? student.removeFirst() : null);
                     break;
 
                 case 3:
-                    Integer expect3=solution.size() > 0 ? solution.removeLast() : null;
-                    Integer actual3=student.size() > 0 ? solution.removeLast() : null;
-                    assertEquals("student is "+actual3+",correct is "+expect3,expect3,actual3);
+                    if (solution.size()>0){
+                        strings.add("removeLast()") ;
+                    }
+
+                    assertEquals(solution.size() > 0 ? solution.removeLast() : null
+                            ,student.size() > 0 ? student.removeLast() : null);
                     break;
 
             }
