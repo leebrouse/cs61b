ce3255de1f120e1ef0aaa82d1c6118a201acd81b
leebrouse <128211841+leebrouse@users.noreply.github.com>
2024-03-09 13:31:29 +0800

    complete proj1ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,68 +1,66 @@
 package tester;
 
 import static org.junit.Assert.*;
+import edu.princeton.cs.introcs.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
-import edu.princeton.cs.introcs.StdRandom;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TestArrayDequeEC {
-    private String generateMsg(List<String> failSeq) {
-        StringBuilder result = new StringBuilder();
-        for (String elem : failSeq) {
-            result.append(elem);
+    private String ErrorShow(List<String> strings){
+        StringBuilder result=new StringBuilder();
+        for (String items:strings){
+            result.append(items);
             result.append("\n");
         }
-        return result.toString();
+        return  result.toString();
     }
 
     @Test
-    public void randomTest() {
-        StudentArrayDeque<Integer> stu_deque = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> solu_deque = new ArrayDequeSolution<>();
-        List<String> failSeq = new ArrayList<>();
-        int n = 100;
-        for (int i = 0; i < n; ++i) {
-            int randNum = StdRandom.uniform(0, 4);
-            switch (randNum) {
+    public void ErrorTest(){
+        StudentArrayDeque<Integer> student= new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution=new ArrayDequeSolution<>();
+        List<String> strings=new ArrayList<>();
+
+        for(int i=0;i<100;i++){
+            int MethodSelector = StdRandom.uniform(0,4);
+            switch (MethodSelector){
                 case 0:
-                    failSeq.add("addFirst(" + i + ")");
-                    stu_deque.addFirst(i);
-                    solu_deque.addFirst(i);
-                    assertEquals(generateMsg(failSeq), solu_deque.get(0), stu_deque.get(0));
-                    break;
+                        strings.add("addFirst("+i+")");
+                        solution.addFirst(i);
+                        student.addFirst(i);
+                        assertEquals(ErrorShow(strings),solution.get(0),student.get(0));
+                        break;
+
                 case 1:
-                    failSeq.add("addLast(" + i + ")");
-                    stu_deque.addLast(i);
-                    solu_deque.addLast(i);
-                    assertEquals(generateMsg(failSeq), solu_deque.get(solu_deque.size() - 1), stu_deque.get(stu_deque.size() - 1));
-                    break;
+                        strings.add("addLast("+i+")");
+                        solution.addLast(i);
+                        student.addLast(i);
+                        assertEquals(ErrorShow(strings),solution.get(solution.size()-1),student.get(student.size()-1));
+                        break;
+
                 case 2:
-                    if (solu_deque.size() > 0) {
-                        failSeq.add("removeLast()");
+                    if (solution.size()>0){
+                        strings.add("removeFirst()") ;
                     }
-                    assertEquals(generateMsg(failSeq), solu_deque.size() > 0 ? solu_deque.removeLast() : null,
-                            stu_deque.size() > 0 ? stu_deque.removeLast() : null);
+
+                    assertEquals(solution.size() > 0 ? solution.removeFirst() : null
+                            ,student.size() > 0 ? student.removeFirst() : null);
                     break;
+
                 case 3:
-                    if (solu_deque.size() > 0) {
-                        failSeq.add("removeFirst()");
+                    if (solution.size()>0){
+                        strings.add("removeLast()") ;
                     }
-                    assertEquals(generateMsg(failSeq), solu_deque.size() > 0 ? solu_deque.removeFirst() : null,
-                            stu_deque.size() > 0 ? stu_deque.removeFirst() : null);
+
+                    assertEquals(solution.size() > 0 ? solution.removeLast() : null
+                            ,student.size() > 0 ? student.removeLast() : null);
                     break;
-                /*case 4:
-                    assertEquals(stu_deque.size() > 0 ? stu_deque.get(0) : null,
-                            solu_deque.size() > 0 ? solu_deque.get(0) : null);
-                    break;
-                case 5:
-                    assertEquals(stu_deque.size(), solu_deque.size());
-                    break;
-                case 6:
-                    assertEquals(solu_deque.isEmpty(), stu_deque.isEmpty());
-                    break;*/
+
             }
         }
     }
+
 }
+
