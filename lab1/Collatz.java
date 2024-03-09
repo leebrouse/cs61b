3b31ef482cb966febd4c9404e826c45d209fc5b8
leebrouse <128211841+leebrouse@users.noreply.github.com>
2024-03-09 14:48:25 +0800

    complete Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,13 +5,11 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        int t=n%2;
+        if(t!=0){
+            return 3*n+1;
         }
+        return n/2 ;
     }
 
     public static void main(String[] args) {
