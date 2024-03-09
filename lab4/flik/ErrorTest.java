49840cd7e8a9767e17d278f424b11d6d8037b14f
leebrouse <128211841+leebrouse@users.noreply.github.com>
2024-03-09 16:12:40 +0800

    complete_lab4

---
+++ lab4/flik/ErrorTest.java
@@ -0,0 +1,19 @@
+package flik;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class ErrorTest {
+    @Test
+    public void Testfilk(){
+        int i=128;
+        int j=128;
+        assertTrue("i should have same value as j",Flik.isSameNumber(i,j));
+    }
+
+
+//    @Test
+//    public void TestHorribleSreve(){
+//
+//    }
+}
--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -6,7 +6,7 @@ public class HorribleSteve {
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
-                        String.format("i:%d not same as j:%d ??", i, j));
+                        String.format("i:%d not same as j:%d ??", i,j));
             }
         }
         System.out.println("i is " + i);
